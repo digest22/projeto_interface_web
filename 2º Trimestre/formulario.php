aff765542752e193c2d6c4935fbcc2e059a4c3e3
Aluno <daniloficha99@gmail.com>
2019-06-24 11:10:25 -0300

    Preencimento do formulario[A[D[B[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[Dh[C[C[C[C[C[C[C[C[C[C[C[C[D[D[D[D[D[D[D[D[D[D[D[Di[C[C[C[C[C[C[C[C[C[C[D[D[D[D[D[D[D[D[D[[C[[A[[3~[ validacao do formulario e preenchimento dos campos

--- 2º Trimestre/formulario.php
+++ 2º Trimestre/formulario.php
@@ -4,6 +4,12 @@
     <meta charset="utf-8">
     <title>Formulários em HTML</title>
     <link rel="stylesheet" href="../CSS/bootstrap.css">
+
+    <!-- Importar o jQuery -->
+<script src="../JS/jquery-3.4.1.js"></script>
+
+    <!-- Importar a validação-->
+    <script src="../JS/validacao.js"></script>
   </head>
   <body>
 
@@ -18,17 +24,19 @@
           method se refere a como esses dados serão enviados.
           - get - pela url
           - post - empacotados -->
-          <form action="formularioControle.php" method="post">
+          <!-- o id formulario será usado para identificar este form -->
+          <form action="formularioControle.php" method="post" id="formulario">
 
             <!-- label serve apenas de etiqueta para o campo -->
             <label>Login:</label><br>
             <!-- Todo input tem que ter um type (tipo) e um name (identificação)
             O name é a identificação do campo lá no action deste formulário -->
-            <input type="text" name="login" value="" class="form-control"><br>
+            <input type="text" name="login" value="" class="form-control obrigatorio">
+            <span class="text-danger"></span><br>
 
             <label>Senha:</label><br>
-            <input type="password" name="senha" value="" class="form-control"><br>
-
+            <input type="password" name="senha" value="" class="form-control obrigatorio">
+            <span class="text-danger"></span><br>
 
             <button type="submit" class="mt-3 btn btn-primary">Entrar</button>
             <br>
---
+++ JS/validacao.js
@@ -0,0 +1,24 @@
+// Espera para ativar o código quando o documento 
+// inteiro terminar de ser carregado
+$(document).ready( function(){
+
+    
+    // Quando o "formulario" for enviado...
+    $("#formulario").submit(function(event){
+        //para cada campo obrigaório...
+        $(".obrigatorio").each(function(){
+            // Armazena o valor do campo
+            var valor = $(this).val();
+            // Se o valor for igual a vazio
+            if(valor == ""){
+                // Coloca uma mensagem no span
+                $(this).next("span").text("Campo obrigatório");
+
+                // Para o envio do formulário
+                event.preventDefault();
+            }
+        }); // fim do each
+
+    }); // fim do submit
+
+}); // fim do ready
